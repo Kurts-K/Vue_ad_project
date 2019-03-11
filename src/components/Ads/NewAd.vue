b674ab0f7358da00eb81c2f8940d3c142f33336b
Kurts-K <35080208+Kurts-K@users.noreply.github.com>
2019-03-11 22:40:20 +0300

    new_ad

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,61 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>New add</h1>
+            <v-flex xs12 sm6 offset-sm-3>
+                <h1 class="text--secondary mb3">Create new ad</h1>
+                <v-form ref="form"
+                        class="mb-3"
+                        v-model="valid"
+                        validation>
+                    <v-text-field
+                            name="title"
+                            label="Ad title"
+                            type="text"
+                            v-model="title"
+                            :rules="[v => !!v || 'Title is required']"
+                            required
+                    ></v-text-field>
+                    <v-text-field
+                            name="description"
+                            label="Ad description"
+                            type="text"
+                            v-model="description"
+                            :rules="[v => !!v || 'Description is required']"
+                            multi-line
+                    ></v-text-field>
+                </v-form>
+                <v-layout row mb-3>
+                    <v-flex xs12>
+                        <v-btn>
+                            Upload
+                            <v-icon right dark>cloud_upload</v-icon>
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <img src="" height="100">
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-switch
+                                v-model="promo"
+                                label="Add to promo"
+                                color="primary"
+                        ></v-switch>
+                    </v-flex>
+                </v-layout>
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                                class="success"
+                                @click="createAd"
+                                :disabled="!valid"
+                        >Create ad</v-btn>
+                    </v-flex>
+                </v-layout>
             </v-flex>
         </v-layout>
     </v-container>
@@ -13,7 +66,23 @@
     export default {
         data() {
             return {
+                title: '',
+                description: '',
+                promo: false,
+                valid: false,
+            }
+        },
+        methods: {
+            createAd() {
+                if(this.$refs.form.validate()) {
+                    const ad = {
+                        title: this.title,
+                        description: this.description,
+                        promo: this.promo,
 
+                    }
+                    console.log(ad);
+                }
             }
         }
     }
--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,36 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Orders</h1>
+                <v-list subheader two-line>
+                    <v-list-tile avatar
+                                 @click=""
+                                 v-for="order in orders"
+                                 :key="order.id"
+                    >
+                        <v-list-tile-action>
+                            <v-checkbox
+                                    color="success"
+                                    :input-value="order.done"
+                                @change="markDone(order)"
+                            ></v-checkbox>
+                        </v-list-tile-action>
+
+                        <v-list-tile-content @click="notifications = !notifications">
+                            <v-list-tile-title>{{order.name}}</v-list-tile-title>
+                            <v-list-tile-sub-title>{{order.fone}}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn raised class="primary"
+                                   color="success"
+                            :to="'/ad/' + order.id"
+                            >Open</v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list>
+
+
             </v-flex>
         </v-layout>
     </v-container>
@@ -13,6 +41,16 @@
     export default {
         data() {
             return {
+                orders: [
+                    {
+                        id: 'id111', name: 'Cos', fone: '5555555', adId: '123', done: false
+                    },
+                ],
+                methods: {
+                    markDone(order) {
+                        order.done = true
+                    }
+                },
 
             }
         }
