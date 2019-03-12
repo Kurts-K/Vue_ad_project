e6f75b10906a2bf30c4fa17158a304791f67ed39
Kurts-K <35080208+Kurts-K@users.noreply.github.com>
2019-03-12 14:35:10 +0300

    firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as fb from 'firebase'
 
 
 
@@ -12,5 +13,15 @@ new Vue({
   router: router,
   store: store,
   render: h => h(App),
+  created() {
+    fb.initializeApp({
+      apiKey: "AIzaSyAJXIEzdSUmChnJSsWJP-9fv4_VFT-yzMg",
+      authDomain: "ad-project-d38f1.firebaseapp.com",
+      databaseURL: "https://ad-project-d38f1.firebaseio.com",
+      projectId: "ad-project-d38f1",
+      storageBucket: "ad-project-d38f1.appspot.com",
+      messagingSenderId: "329135162027"
+    })
+  }
 
 }).$mount('#app')
